0844d9d62516cfb676a8dfa4cf747d5f0c57c864
chester777 <aster9797@gmail.com>
2016-11-06 23:23:49 +0900

    Generate Json Object and Request to Azure Server

--- MinorController.py
+++ MinorController.py
@@ -1,7 +1,25 @@
 import json
+import time
+import httplib
 
-device = {
-	'deviceNo': 1,
-	'deviceType': 'status',
-	'timestamp': 
+deviceInfo = {
+	"deviceNo": 1,
+	"deviceType": "status",
+	"timestamp": time.time()
 }
+
+deviceInfoToJson = json.dumps(deviceInfo)
+
+headers = {
+	"Content-type": "application/x-www-form-jsonencoded",
+	"Accept": "json"
+}
+
+conn = httplib.HTTPConnection("40.74.138.192:80")
+conn.request("POST", "json_get.php", deviceInfoToJson, headers)
+response = conn.getresponse()
+
+print response.status, response.reason
+
+data = reason.read()
+conn.close()
