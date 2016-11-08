8a3254499ce305ee2ebf5da8999cd3b6032f0e3a
chester777 <aster9797@gmail.com>
2016-11-08 18:36:31 +0900

    BLE Scanner Added

--- MinorController.py
+++ MinorController.py
@@ -2,11 +2,33 @@ import json
 import time
 import httplib
 import urllib
+import blescan
+import sys
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+try:
+    sock = bluez.hci_open_dev(dev_id)
+    print "ble thread started"
+
+except:
+    print "error accessing bluetooth device..."
+    sys.exit(1)
+
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+
+while True:
+    returnedList = blescan.parse_events(sock, 10)
+    print "----------"
+    for beacon in returnedList:
+        print beacon	
 
 deviceInfo = {
 	"deviceNo": 1,
-	"deviceType": "status",
-	"timestamp": time.time()
+	"deviceName": "GasLock"
+	"timestamp": time.time(),
+	"status": 1
 }
 
 jsonServerUrl = "40.74.138.192"
