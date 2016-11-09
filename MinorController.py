f80a158193b782b8b8e37dd5be0167dad961f288
chester777 <aster9797@gmail.com>
2016-11-10 03:14:28 +0900

    fixed bug

--- MinorController.py
+++ MinorController.py
@@ -44,8 +44,7 @@ if __name__ == '__main__' :
 				if row != None :
 
 					currentTime = time.time()
-					type(row[0])
-					cur.execute("INSERT INTO log VALUES(" + row[0] + "," + currentTime + "," + MAJOR + "," + MINOR +")")
+					cur.execute("INSERT INTO log VALUES(" + row[0] + "," + str(currentTime) + "," + str(MAJOR) + "," + str(MINOR) +")")
 
 					deviceInfo["deviceNo"] = row[0]
 					deviceInfo["deviceName"] = row[1]
