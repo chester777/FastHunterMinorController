4d88abb898297a746ddb8c0f1c6e71692cc7a5b3
chester777 <aster9797@gmail.com>
2016-11-10 02:49:18 +0900

    fixed indent

--- MinorController.py
+++ MinorController.py
@@ -14,12 +14,12 @@ deviceUuidRegEx = r"[a-zA-Z0-9]{32}"
 if __name__ == '__main__' :
 	dev_id = 0
 	try:
-	    sock = bluez.hci_open_dev(dev_id)
-	    print "ble thread started"
+		sock = bluez.hci_open_dev(dev_id)
+		print "ble thread started"
 
 	except:
-	    print "error accessing bluetooth device..."
-	    sys.exit(1)
+		print "error accessing bluetooth device..."
+		sys.exit(1)
 
 	blescan.hci_le_set_scan_parameters(sock)
 	blescan.hci_enable_le_scan(sock)
@@ -28,24 +28,24 @@ if __name__ == '__main__' :
 	cur = conn.cursor()
 
 	while True:
-	    returnedList = blescan.parse_events(sock, 1)
-	    print "----------"
-	    for beacon in returnedList:
+		returnedList = blescan.parse_events(sock, 1)
+		print "----------"
+		for beacon in returnedList:
 
-	    	MAC_ADDR = beacon.split(",")[0]
-	    	UUID = beacon.split(",")[1]
-	    	MAJOR = beacon.split(",")[2]
-	    	MINOR = beacon.split(",")[3]
+			MAC_ADDR = beacon.split(",")[0]
+			UUID = beacon.split(",")[1]
+			MAJOR = beacon.split(",")[2]
+			MINOR = beacon.split(",")[3]
 
-	    	cur.execute("SELECT deviceNo, deviceName FROM deviceInfo")
+			cur.execute("SELECT deviceNo, deviceName FROM deviceInfo")
 
-	    	rows = cur.fetchall()
-	    	for row in rows :
-	    		if row != None :
+			rows = cur.fetchall()
+			for row in rows :
+				if row != None :
 
-	    			currentTime = time.time()
-	    			cur.execute("INSERT INTO log VALUES(" + row[0] + "," + currentTime + "," + MAJOR + "," + MINOR +")")
-	    			
+					currentTime = time.time()
+					cur.execute("INSERT INTO log VALUES(" + row[0] + "," + currentTime + "," + MAJOR + "," + MINOR +")")
+
 					deviceInfo["deviceNo"] = row[0]
 					deviceInfo["deviceName"] = row[1]
 					deviceInfo["timestamp"]	= currentTime
